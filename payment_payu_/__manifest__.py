f12ebe633a8beca203da18467d38cc6a01966ab2
Prajjwal Tripathi <ptripathi@boxpay.tech>
2025-09-08 12:07:38 +0530

    [MODIF] modified 'summary' in the manifest file for adding search  keywords, decreased 'sequence' attribute to make addon appear closer to beggining inside odoo app list.

--- payment_payu_/__manifest__.py
+++ payment_payu_/__manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'PayU Payment Provider',
-    'version': '18.0.0.0',
+    'version': '1.0',
     'category': 'Accounting/Payment Providers',
-    'sequence': 350,
-    'summary': 'Payment Provider: PayU Integration',
+    'sequence': 1,
+    'summary': 'PayU Payment Provider addon enables seamless integration of PayU Hosted Checkout with all payment methods. It offers merchants a secure and flexible payment solution to process payments efficiently within Odoo.',
     'description': "This module provides the integration of PayU as a payment provider in Odoo.",
     'icon': '/payment_payu/static/description/icon.svg',
     'author': 'PayU',
@@ -13,8 +13,8 @@
         'views/payment_payu_templates.xml',
         'views/payment_provider_views.xml',
         'data/payment_provider_data.xml',
+        'security/ir.model.access.csv'
     ],
-    'images' : ['static/description/banner.png'],
     'test': ['tests/test_payment_provider.py',
              'tests/test_payment_transaction.py'],
     'post_init_hook': 'post_init_hook',
